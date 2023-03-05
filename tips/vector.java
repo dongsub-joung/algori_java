487c94292c80fad24715bf672518a3c4193c5ca9
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-03-05 13:18:46 +0900

    vector

---
+++ tips/vector.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Vector;
+
+public class Main {
+    public static void main(String[] args) {
+        // thread safe
+        Vector<Integer> integerVector= new Vector<>();
+
+        ArrayList<Integer> integerArrayList= new ArrayList<>();
+        LinkedList<Integer> integerLinkedList= new LinkedList<>();
+
+    }
+}
